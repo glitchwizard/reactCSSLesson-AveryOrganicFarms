8e4fd9247974961662b5c9d12efd6b6bb4e4906c
Charley McGowan and Jared Reando <charley@>
2019-02-12 14:29:01 -0800

    Add responsive scaling for grid layout.

--- src/components/ProduceMonth.jsx
+++ src/components/ProduceMonth.jsx
@@ -7,14 +7,57 @@ const fullPage = {
   height: '100vh',
   background: 'tan',
   display: 'grid',
-  gridTemplate: 'repeat(3, 1fr) / repeat(4, 1fr)',
-  border: '1px solid black'
+  gridColumnGap: '20px',
+  gridRowGap: '20px',
+  gridTemplateColumns: 'repeat(4, 1fr)',
+  gridTemplateRows: 'repeat(3, calc(1/3vh - 20px)',
+  border: '1px solid black',
+  overflow: 'auto',
+  border: '5px red dashed'
 };
 
 function ProduceMonth() {
   return (
-    <div style={fullPage}>
+    <div className='red-text' >
+      <style jsx>{`
+        @media (min-width: 1451px) {
+          .red-text {
+            display: grid;
+            grid-template-columns: repeat(4, 1fr);
+            grid-template-rows: repeat(3, 1fr);
+            color: blue;
+          }
+        }
 
+        @media (max-width: 1450px) {
+          .red-text {
+            display: grid;
+            grid-template-columns: repeat(3, 1fr);
+            grid-template-rows: repeat(4, 1fr);
+            color: blue;
+          }
+        }
+
+        @media (max-width: 1150px) {
+          .red-text {
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            grid-template-rows: repeat(6, 1fr);
+            color: blue;
+          }
+        }
+
+        @media (max-width: 600px) {
+          .red-text {
+            display: grid;
+            grid-template-columns: repeat(1, 1fr);
+            grid-template-rows: repeat(12, 1fr);
+            color: red;
+          }
+        }
+
+      `}
+      </style>
       {data.availableProduce.map((monthObject, index)  =>
         <SeasonalOfferings
         month = {monthObject.month}
--- src/components/SeasonalOfferings.jsx
+++ src/components/SeasonalOfferings.jsx
@@ -2,15 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function SeasonalOfferings({month, selection}) {
+  const autoGrid = {
+    overflow: 'auto',
+    border: '1px black solid',
+    // display: 'grid',
+    // gridTemplateColumns: 'repeat(7, 10px)',
+    // gridAutoRows: 'fit-content(1fr)'
+  }
+  const listDisplayGridItem = {
+    border: '2px blue solid',
+    backgroundColor: 'lightgreen',
+    display: 'block',
+    width: '99%',
+    position:'sticky',
+    left: '0',
+    top: '0'
+  }
+  const itemList = {
+    display: 'grid',
+    gridTemplate: 'repeat(12, 20px) / repeat(3, 1fr)'
+  }
   return (
-    <div>
-      <h3>{month}</h3>
-      <ul>
+    <div style={autoGrid}>
+      <div style={listDisplayGridItem}>
+        <h3 style={{textAlign: 'center'}}>{month}</h3>
+      </div>
+      <div style={itemList}>
         {selection.map((selectionItem, index) =>
-          <li key={index}>{selectionItem} </li>
+          <p key={index}>- {selectionItem} </p>
         )}
-      </ul>
-      <hr/>
+      </div>
     </div>
   );
 }
